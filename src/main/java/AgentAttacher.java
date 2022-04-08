67b605bf1623df65eb16636458e5cd98d8c842c8
Omen <89894310+OmenDoesStuff@users.noreply.github.com>
2022-04-08 10:54:01 -0400

    Update AgentAttacher.java

--- src/main/java/AgentAttacher.java
+++ src/main/java/AgentAttacher.java
@@ -3,6 +3,9 @@ public class AgentAttacher {
      String pid;
   
      public static void main(String[] args) {
-       
+       if(args.length < 1) {
+         System.out.println("Usage: java -jar AgentAttacher.jar <pid>"); 
+         System.exit(0);    
+       }
      }
 }
