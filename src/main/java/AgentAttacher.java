a3d887d8156574d370f929f6e0083d29f029a8d6
Omen <89894310+OmenDoesStuff@users.noreply.github.com>
2022-04-08 11:47:07 -0400

    Update AgentAttacher.java

--- src/main/java/AgentAttacher.java
+++ src/main/java/AgentAttacher.java
@@ -2,11 +2,13 @@ import java.lang.management.ManagementFactory;
 
 import com.sun.tools.attach.VirtualMachine;
 
+import java.lang.*;
+
 public class AgentAttacher {
      public static String pid;
      public static String path;
   
-     public static void main(String[] args) {
+     public static void main(String[] args) throws Exception {
        if(args.length < 2) {
          System.out.println("Usage: java -jar AgentAttacher.jar <pid> <agentPath>"); 
          System.exit(0);    
@@ -21,7 +23,7 @@ public class AgentAttacher {
      }
 
      
-     public static void attachAgent(String jvmPid, String agentPath) {
+     public static void attachAgent(String jvmPid, String agentPath) throws Exception {
       
        pid = jvmPid;
        path = agentPath;
